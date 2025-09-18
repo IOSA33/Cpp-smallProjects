9f86afadc9bc2cf9ab25ce40d6843a2bae5df5ac
IOSA33 <sedinkin33@gmail.com>
2025-09-18 22:23:23 +0300

    added robot for playing

--- tic-tac-toe/tictactoe.cpp
+++ tic-tac-toe/tictactoe.cpp
@@ -1,27 +1,54 @@
 #include "tictactoe.h"
-#include <list>
-#include <string>
 #include <iostream>
 #include <bits/ostream.tcc>
+#include <random>
+#include <ctime>
 
-void printTable(int arr[][3], int);
-int updateTable(int arr[][3], int, int);
+
+void printTable(int arr[][3], int rows);
+int updateTable(int arr[][3], int inputX, int inputY, int value);
 
 constexpr int rowsGame{3};
+constexpr int valuePlayer{1};
+constexpr int valueRobot{5};
 
 int main() {
     std:: cout << "Hello it is a mini game tic-tac-toe:"<< "\n";
+
+    // Array of our map game
     int array[3][3] = {{0,0,0}, {0,0,0}, {0,0,0}};
     printTable(array, rowsGame);
 
     while (true) {
         std::cout << "Enter a number for X and Y, format (0 2):" << "\n";
+        // User inputs
         int inputX{};
         int inputY{};
         std::cin >> inputX >> inputY;
 
+        // Game starts
         if (inputX >= 0 && inputY >= 0 && inputX < 3 && inputY < 3) {
-            updateTable(array, inputX, inputY);
+            // Users move, and print table after input
+            updateTable(array, inputX, inputY , valuePlayer);
+            printTable(array, rowsGame);
+
+            // For robot inputs
+            int inputX1{};
+            int inputY2{};
+
+            // Random generator for robot by random library
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> dist(0, 2);
+
+            inputX1 = dist(gen);
+            inputY2 = dist(gen);
+
+            // Updating table and printing it again
+            std::cout << "Robot makes move" << std::endl;
+            std::cout << "My move is " << inputX1 << " " << inputY2 << "\n";
+
+            updateTable(array, inputX1, inputY2, valueRobot);
             printTable(array, rowsGame);
         } else {
             std::cout<< "Invalid input! Try again! \n";
@@ -29,7 +56,9 @@ int main() {
             std::cin.ignore(1000, '\n');
         }
 
-        break;
+        if (inputX == 10 || inputY == 10) {
+            break;
+        }
     }
 
     return 0;
@@ -44,9 +73,9 @@ void printTable(int arr[][3], const int rows) {
     }
 }
 
-int updateTable(int arr[][3], int inputX, int inputY) {
+int updateTable(int arr[][3], const int inputX, const int inputY, int value) {
     if (inputX >= 0 && inputY >= 0 && inputX < 3 && inputY < 3) {
-        return arr[inputX][inputY] = 1;
+        return arr[inputX][inputY] = value;
     }
     return 0;
 }
