4866169203a2b26d67299c873c62ec5412187452
IOSA33 <sedinkin33@gmail.com>
2025-09-25 20:17:36 +0300

    added better test handler

--- chapters/chapter9/debugging.cpp
+++ chapters/chapter9/debugging.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-bool isLowerVowel(char c) {
-    switch (c) {
+bool isLowerVowel(char c)
+{
+    switch (c)
+    {
         case 'a':
         case 'e':
         case 'i':
@@ -13,16 +15,38 @@ bool isLowerVowel(char c) {
     }
 }
 
-// Now we added test cases for later. So basically here if we want to retest things later
+// So basically here if we want to retest things later
+// This requires you to remember what the expected answer was at worst (assuming you didn’t document it),
+// and manually compare the actual results to the expected results. This is not what we wanted.
 void testisLowerVowel() {
     std::cout << isLowerVowel('a') << '\n'; // temporary test code, should produce 1
     std::cout << isLowerVowel('q') << '\n'; // temporary test code, should produce 0
+}
+
+// We can do better by writing a test function that contains both the tests
+// AND the expected answers and compares them so we don’t have to.
+// returns the number of the test that failed, or 0 if all tests passed
+int testVowel()
+{
+    if (!isLowerVowel('o')) return 1;
+    if (isLowerVowel('q')) return 2;
+    if (!isLowerVowel('e')) return 3;
+
+    // Returns 0 if all tests passed
+    return 0;
 }
 
 
 int main() {
     // Returning 1, because it is true
     std::cout << isLowerVowel('a') << "\n";
+    testisLowerVowel();
 
-    return EXIT_SUCCESS;
+    int result { testVowel() };
+    if (result != 0)
+        std::cout << "testVowel() test " << result << " failed.\n";
+    else
+        std::cout << "testVowel() tests passed.\n";
+
+    return 0;
 }
