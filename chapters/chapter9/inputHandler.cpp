73c32df17bd2a0560861c2f2c7f66d20daf2fb7b
IOSA33 <sedinkin33@gmail.com>
2025-09-27 18:43:55 +0300

    the good way to handle the invalid input

--- chapters/chapter9/inputHandler.cpp
+++ chapters/chapter9/inputHandler.cpp
@@ -8,7 +8,29 @@ void ignoreLine() {
 // Another method to check if the last input is \n or something else, for std::cin
 // has extraneous input:
 bool hasUnextractedInput() {
+    // An end-of-file (EOF)
     return !std::cin.eof() && std::cin.peek() != '\n';
+}
+
+// returns true if extraction failed, false otherwise
+bool clearFailedExtraction()
+{
+    // Check for failed extraction
+    if (!std::cin) // If the previous extraction failed
+    {
+        if (std::cin.eof()) // If the user entered an EOF
+        {
+            std::exit(0); // Shut down the program now
+        }
+
+        // Let's handle the failure
+        std::cin.clear(); // Put us back in 'normal' operation mode
+        ignoreLine();     // And remove the bad input
+
+        return true;
+    }
+
+    return false;
 }
 
 double getDouble()
@@ -29,6 +51,7 @@ double getDouble()
         // Now we check if cin is failure ofr example input for example 'a', because cin cant convert the
         // A to a decimal that's why cin enters the infinite loop in the buffer for trying everytime the convert the 'a'
         // We are telling "if the cin is failure enter the if statement". This will work also "if (std::cin.fail())"
+        /*
         if (!std::cin) {
             // "clear the error state" of cin
             std::cin.clear(); // Put us back in 'normal' operation mode
@@ -36,8 +59,26 @@ double getDouble()
             continue;
         }
 
+        */
+
+        // The better way to handle input
+        if (clearFailedExtraction()) {
+            std::cout << "Oops, that input is invalid.  Please try again.\n";
+            continue;
+        }
+
         ignoreLine();
         return x;
+
+        /*
+         *This is the same way that is above but more simplier
+        bool success { std::cin }; // Remember whether we had a successful extraction
+        std::cin.clear();          // Put us back in 'normal' operation mode (in case we failed)
+        ignoreLine();              // Ignore any additional input on this line (regardless)
+
+        if (success)               // If we actually extracted a value
+            return x;              // Return it (otherwise, we go back to top of loop)
+         */
     }
 }
 
@@ -48,6 +89,9 @@ char getOperator()
         std::cout << "Enter one of the following: +, -, *, or /: ";
         char operation{};
         std::cin >> operation;
+
+        if (!clearFailedExtraction()) // we'll handle error messaging if extraction failed below
+            ignoreLine(); // remove any extraneous input (only if extraction succeded)
 
         // Check whether the user entered meaningful input
         switch (operation)
@@ -81,8 +125,9 @@ void printResult(double x, char operation, double y)
         case '/':
             std::cout << x / y << '\n';
             return;
-
     }
+
+    std::cout << "???";  // Being robust means handling unexpected parameters as well, even though getOperator() guarantees operation is valid in this particular program
 }
 
 int main()
@@ -90,6 +135,13 @@ int main()
     double x{ getDouble() };
     char operation{ getOperator() };
     double y{ getDouble() };
+
+    // Handle division by 0
+    while (operation == '/' && y == 0.0)
+    {
+        std::cout << "The denominator cannot be zero.  Try again.\n";
+        y = getDouble();
+    }
 
     printResult(x, operation, y);
 
