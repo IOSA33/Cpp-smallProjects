d1eb8d60e5b3198910a64592ac023f0b170d1b8f
IOSA33 <sedinkin33@gmail.com>
2025-10-01 14:02:19 +0300

    function overloading

---
+++ chapters/chapter11/func-overloading.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//Function overloading provides a great way to reduce the
+//complexity of your program by reducing the number of function
+//names you need to remember. It can and should be used liberally.
+
+// We can use same name, but difference is in parameters
+int add(int x, int y) {
+    return x + y;
+}
+
+double add(double x, double y) {
+    return x + y;
+}
+
+int main() {
+    add(1, 2);
+    std::cout<< add(3.53, 4.353);
+    return 0;
+}
