8ade1d793f6a6f5b9756274277b7f9cc6be91e27
IOSA33 <sedinkin33@gmail.com>
2025-10-13 13:25:36 +0300

    added serch in binary tree

--- chapters/chapter12/binaryTreeCalculate.cpp
+++ chapters/chapter12/binaryTreeCalculate.cpp
@@ -9,6 +9,29 @@ public:
     Node(T value) : data(value), left(nullptr), right(nullptr) {}
 };
 
+template<typename T>
+class BinaryTree {
+private:
+    // Pointer to the root of the tree
+    Node<T>* root;
+
+    // Helper Function to find the minimum value node
+    Node<T>* findMin(Node<T>* node) {
+        while (node->left != nullptr) {
+            node = node->left;
+        }
+        return node;
+    }
+
+    // Recursive Function to search for a value in the tree
+    bool serchRecursive(Node<T>* current, T value) {
+        if (current == nullptr) return false;
+        if (current->data == value) return true;
+        return serchRecursive(current->left, value) || serchRecursive(current->right, value);
+    }
+
+};
+
 int main() {
     return 0;
 }
