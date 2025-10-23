02f7c7553608aa9f4b51c5c6e6d0993b9a56c0d9
IOSA33 <sedinkin33@gmail.com>
2025-10-23 19:35:01 +0300

    null pointer

--- chapters/chapter12/pointers.cpp
+++ chapters/chapter12/pointers.cpp
@@ -5,5 +5,22 @@
 
 
 int main() {
-    char x {};
+    int x{5};
+    int& ref{x};
+    int* ptr;
+    int* ptr2{ nullptr }; // NUll pointer, best practice
+    int* ptr3{&x};
+
+    int* ptr4 {&x};
+    ptr4 = nullptr;
+
+    someFunction(nullptr);
+
+    std::cout << &x << std::endl;
+    std::cout << ptr << std::endl;
+    std::cout << ptr2 << std::endl;
+    std::cout << ptr3 << std::endl;
+    std::cout << *ptr3 << std::endl;
+
+    return 0;
 }
