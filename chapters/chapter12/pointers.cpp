591a0f6a8db642b8d5a768addc6dafdcbc580a0d
IOSA33 <sedinkin33@gmail.com>
2025-10-23 20:14:39 +0300

    const pointers

--- chapters/chapter12/pointers.cpp
+++ chapters/chapter12/pointers.cpp
@@ -24,6 +24,22 @@ int main() {
         std::cout << "ptr is non-null\n";
 
 
+    // Dangling pointer example.
+    int* ptr6 {};
+    {
+        int newDanglingValue{12};
+        ptr6 = &newDanglingValue;
+    }  // ptr is now dangling and pointing to invalid object
+
+    if (ptr) // condition evaluates to true because ptr is not nullptr
+        std::cout << *ptr; // undefined behavior
+
+    int v{5};
+    int* ptr0 { &v };             // points to an "int" but is not const itself.  We can modify the value or the address.
+    const int* ptr10 { &v };       // points to a "const int" but is not const itself.  We can only modify the address.
+    int* const ptr20 { &v };       // points to an "int" and is const itself.   We can only modify the value.
+    const int* const ptr30 { &v }; // points to a "const int" and is const itself.  We can't modify the value nor the address.
+
     std::cout << &x << std::endl;
     std::cout << ref << std::endl;
     std::cout << ptr2 << std::endl;
