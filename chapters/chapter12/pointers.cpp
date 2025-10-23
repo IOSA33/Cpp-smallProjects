e6338e9a2e81d4f0690573e232a957af58aa949c
IOSA33 <sedinkin33@gmail.com>
2025-10-23 19:47:16 +0300

    testing null pointer

--- chapters/chapter12/pointers.cpp
+++ chapters/chapter12/pointers.cpp
@@ -14,12 +14,23 @@ int main() {
     int* ptr4 {&x};
     ptr4 = nullptr;
 
-    someFunction(nullptr);
+    // explicit test for equivalence
+    if (ptr2 == nullptr || ptr3 == nullptr)
+        std::cout << "ptr is null\n";
+    else
+        std::cout << "ptr is non-null\n";
+
+    if (!ptr2) // Boolean value
+        std::cout << "ptr is non-null\n";
+
 
     std::cout << &x << std::endl;
-    std::cout << ptr << std::endl;
+    std::cout << ref << std::endl;
     std::cout << ptr2 << std::endl;
     std::cout << ptr3 << std::endl;
+    //  Dereferencing a pointer means
+    //  “go to the address the pointer is pointing at
+    //  and access the value there”
     std::cout << *ptr3 << std::endl;
 
     return 0;
