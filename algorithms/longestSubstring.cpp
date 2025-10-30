81eb7682df225fc35c00fba804cf7e00b8a4a4e0
IOSA33 <sedinkin33@gmail.com>
2025-10-30 23:12:52 +0200

    blind75 forloop

--- algorithms/longestSubstring.cpp
+++ algorithms/longestSubstring.cpp
@@ -4,23 +4,32 @@
 class Solution {
 public:
     static int lengthOfLongestSubstring(const std::string& s) {
+        if (s.empty()) {
+            return 0;
+        }
+
+        if (s == " ") {
+            return 1;
+        }
+
         int result{0};
         int temp{0};
 
         std::unordered_set<char> hashSet;
 
         for (int i = 0; i < s.length(); ++i) {
-            if (hashSet.find(s[i]) == hashSet.end()) {
-                ++temp;
-                hashSet.insert(s[i]);
-            } else {
+            for (int j = i; j < s.length(); ++j) {
+                if (hashSet.find(s[j]) == hashSet.end()) {
+                    ++temp;
+                    hashSet.insert(s[j]);
+                } else {
+                    temp = 0;
+                    hashSet.erase(hashSet.begin(), hashSet.end());
+                    break;
+                }
                 if (temp > result) {
                     result = temp;
                 }
-                temp = 0;
-                hashSet.erase(hashSet.begin(), hashSet.end());
-                hashSet.insert(s[i]);
-                ++temp;
             }
         }
 
@@ -30,7 +39,7 @@ public:
 
 
 int main() {
-    const std::string s = "pwwwkew";
+    const std::string s = "dvdf";
 
     std::cout <<  Solution::lengthOfLongestSubstring(s) << std::endl;
 }
