c93cf71af09f4247557896f3688bd8be98dd690f
IOSA33 <sedinkin33@gmail.com>
2025-11-01 20:56:08 +0200

    enums, switch case

--- chapters/chapter13/enums.cpp
+++ chapters/chapter13/enums.cpp
@@ -6,9 +6,12 @@ enum SortOrder {
     numerical,
 };
 
-void sortData(SortOrder data) {
-    switch (data) {
+void sortData(int data[3], const SortOrder order) {
+    switch (order) {
         case alphabetical:
+            for (int i = 0; i < 4; i++) {
+                std::cout << data[i] << '\n';
+            }
             break;
         case alphabeticalReverse:
             break;
@@ -18,5 +21,9 @@ void sortData(SortOrder data) {
 }
 
 int main() {
+    int arr[4] = {1, 2, 3, 4};
 
+    sortData(arr, alphabetical);
+
+    return EXIT_SUCCESS;
 }
