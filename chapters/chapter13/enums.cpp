3702c9f4e06a9268266b52cb77e3d75f10624b3f
IOSA33 <sedinkin33@gmail.com>
2025-11-01 20:44:30 +0200

    enums in practice

---
+++ chapters/chapter13/enums.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+enum SortOrder {
+    alphabetical,
+    alphabeticalReverse,
+    numerical,
+};
+
+void sortData(SortOrder data) {
+    switch (data) {
+        case alphabetical:
+            break;
+        case alphabeticalReverse:
+            break;
+        case numerical:
+            break;
+    }
+}
+
+int main() {
+
+}
