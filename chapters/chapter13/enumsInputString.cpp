3ac60f341ce0b7fcc47ab871d4f186ef9c16a01c
IOSA33 <sedinkin33@gmail.com>
2025-11-03 16:24:09 +0200

    enums input String 2

--- chapters/chapter13/enumsInputString.cpp
+++ chapters/chapter13/enumsInputString.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string_view>
+#include <optional>
+#include <string>
 
 enum Color {
     black,
@@ -20,12 +22,35 @@ constexpr std::string_view getColor(Color color) {
     }
 }
 
+constexpr std::optional<Color> getColorFromString(std::string_view color) {
+    if (color == "black") return black;
+    if (color == "blue") return blue;
+    if (color == "red") return red;
+    return {};
+}
+
 int main() {
     constexpr Color shirt{ blue };
-
     std::cout << shirt << '\n'; // Prints 1
+    std::cout << getColor(shirt) << std::endl; // Prints blue
 
-    std::cout << getColor(shirt) << std::endl;
+    int input{};
+    // std::cin >> input;
+    if (input < 0 || input > 2) {
+        std::cout << "Wrong integer" << '\n';
+    } else {
+        Color tshirt{ static_cast<Color>(input) };
+        std::cout << getColor(tshirt) << '\n';
+    }
+
+    std::string inputString{};
+    std::cin >> inputString;
+    std::optional<Color> color {getColorFromString(inputString)};
+    if (color) {
+        std::cout << "You entered an: " << getColor(*color) << '\n';
+    } else {
+        std::cout << "You entered an invalid string input" << '\n';
+    }
 
     return EXIT_SUCCESS;
 }
