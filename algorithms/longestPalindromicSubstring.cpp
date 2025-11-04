5bc8cdf25d87ef167a26aac070d16994489a003b
IOSA33 <sedinkin33@gmail.com>
2025-11-04 21:04:20 +0200

    fixed algroithm with two pointers

--- algorithms/longestPalindromicSubstring.cpp
+++ algorithms/longestPalindromicSubstring.cpp
@@ -1,45 +1,36 @@
 #include <iostream>
-#include <unordered_set>
-#include <vector>
+#include <string>
+
+bool check(std::string& s, int i, int j) {
+    int left = i;
+    int right = j - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+
+        ++left;
+        --right;
+    }
+
+    return true;
+}
 
 std::string longestPalindrome(std::string& s) {
-    std::string result;
-
-    std::unordered_set<std::string> m_set;
-
-    for (int i = 0; i <= s.length(); ++i) {
-        for (int j = 1; j <= s.length() - i; ++j) {
-            std::string temp = s.substr(i, j);
-            if (temp.length() > 1) {
-                m_set.insert(s.substr(i, j));
+    for (int length = s.size(); length > 0; length--) {
+        for (int start = 0; start <= s.size() - length; start++) {
+            if (check(s, start, start + length)) {
+                return s.substr(start, length);
             }
         }
     }
 
-    for (int i = 0; i <= s.length(); ++i) {
-        for (int j = s.length() - 1; j >= 0; --j) {
-            std::string temp = s.substr(i, j + 1);
+    return "";
 
-            if (temp.length() > 1) {
-                std::string temp1;
-                // for (char & it : std::ranges::reverse_view(temp))
-                for (auto it = temp.rbegin(); it != temp.rend(); ++it) {
-                    temp1 += *it;
-                }
-
-                if (m_set.find(temp1) != m_set.end()) {
-                    if (temp1.length() > result.length()) {
-                        result = temp1;
-                    }
-                }
-            }
-        }
-    }
-
-    return result;
 }
 
 int main() {
-    std::string s = "babas";
+    std::string s = "aacabdkacaa";
     std::cout << longestPalindrome(s) << std::endl;
 }
