28e6479ebcb9745d04ef1d2a2af39f4b21f0dfbb
IOSA33 <sedinkin33@gmail.com>
2025-11-06 21:23:32 +0200

    structs

--- chapters/chapter13/structs.cpp
+++ chapters/chapter13/structs.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 
 struct Employee {
+    int x;       // no default initialization value (bad)
+    int y {};    // value-initialized by default
+    int z { 2 }; // explicit default value, already with value
+
     int id{}; // data members
     int age{};
+
     double wage{};
 };
 
+std::ostream& operator<<(std::ostream& out, const Employee& e) {
+    out << "id: " << e.id << " Age: " << e.age << " Wage: "<< e.wage;
+    return out;
+}
+
 int main() {
-    Employee joe {};
+
+    // Best practice:
+    // For aggregates, prefer value initialization (with an empty braces initializer)
+    // to default initialization (with no braces). emp is bad example, prefer do not do that.
+
+    // Default initialization
+    Employee emp; //   int x; will not initialize
+    // The value initialization
+    Employee joe {};// This is safer, because it prevents us from missing values: int x; will be 0
     joe.id = 14;
     joe.age = 32;
     joe.wage = 60000.0;
@@ -17,8 +35,12 @@ int main() {
     frank.age = 28;
     frank.wage = 45000.0;
 
+    Employee joy {.id = 1, .age = 1, .wage = 2}; // c++20
+    Employee joy2 {joy}; // direct-list-initialization
+
     int totalAge {joe.age + frank.age};
     std::cout << totalAge << std::endl;
+    std::cout << joe << std::endl;
 
     if (joe.wage > frank.wage)
         std::cout << "Joe makes more than Frank\n";
