6de96594c7977b7a51c03dfbaa91afb7f344478b
IOSA33 <sedinkin33@gmail.com>
2025-11-13 17:43:22 +0200

    multiple contructions

---
+++ chapters/chapter14/classExersice2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Ball {
+    std::string m_color{ "black" };
+    double m_radius{10.0};
+
+public:
+    Ball() = default;
+    Ball(double radius) : m_radius(radius) {
+        print();
+    }
+    Ball(std::string_view color) : m_color(color) {
+        print();
+    }
+    Ball(std::string_view color, double radius) : m_color(color), m_radius(radius) {
+        print();
+    }
+
+    void print() const
+    {
+        std::cout << "Ball(" << m_color << ", " << m_radius << ")\n";
+    }
+};
+
+int main() {
+    Ball def{};
+    Ball blue{ "blue" };
+    Ball twenty{ 20.0 };
+    Ball blueTwenty{ "blue", 20.0 };
+
+    return 0;
+}
