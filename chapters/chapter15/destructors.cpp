544cae00e465e0c9c2170d0d7eaf79b84d4998c9
IOSA33 <sedinkin33@gmail.com>
2025-11-14 18:29:42 +0200

    destructor over Network ex

---
+++ chapters/chapter15/destructors.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class NetworkData {
+    std::string m_serverName{};
+    DataStore m_dataQueue{};
+
+public:
+    NetworkData(std::string_view serverName) : m_serverName(serverName) {}
+    ~NetworkData() {
+        sendData();
+    }
+
+    void addData(std::string_view data) {
+        m_dataQueue.add(data);
+    }
+
+    void sendData() {
+        // connect to server
+        // send all data
+        // clear data
+    }
+};
+int main()
+{
+    NetworkData n("someipAddress");
+
+    n.addData("somedata1");
+    n.addData("somedata2");
+
+    return 0;
+}
