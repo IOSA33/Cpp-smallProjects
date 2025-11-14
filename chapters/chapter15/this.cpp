1e00623f5a41e7841ea6bfe64e7935623ab815bd
IOSA33 <sedinkin33@gmail.com>
2025-11-14 14:56:11 +0200

    chainable functions with *this

--- chapters/chapter15/this.cpp
+++ chapters/chapter15/this.cpp
@@ -15,12 +15,17 @@ public:
     // static void setID(Simple* const this, int id) { this->m_id = id; }
 
     void print() const { std::cout << this->m_id; } // use `this` pointer to access the implicit object and operator-> to select member m_id
+
+    // Returning *this for doing chainable function
+    Simple& add(int value) {m_id += value; return *this;}
+    Simple& sub(int value) {m_id -= value; return *this;}
 };
 
 struct Something {
     int data{}; // member without m_, cause struct
 
     void setData(int data) {
+    // because the function parameter shadows the data member
         this->data = data; // using this for disambiguate data parameter from member
     }
 };
@@ -29,6 +34,9 @@ int main() {
     Simple simple{ 1 };
     simple.setID(2);
     // Compiler rewrites it as: Simple::setID(&simple, 2)
+
+    // Chaining method
+    simple.add(3).sub(4);
 
     simple.print();
 
