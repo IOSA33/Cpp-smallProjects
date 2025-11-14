7577b8917a1e7f6bb452e8394f3c1043b9004377
IOSA33 <sedinkin33@gmail.com>
2025-11-14 14:46:01 +0200

    added when this is useful to use

--- chapters/chapter15/this.cpp
+++ chapters/chapter15/this.cpp
@@ -11,8 +11,18 @@ public:
 
     int getID() const { return m_id; }
     void setID(int id) { m_id = id; }
+    // Compiler rewrites it as:
+    // static void setID(Simple* const this, int id) { this->m_id = id; }
 
     void print() const { std::cout << this->m_id; } // use `this` pointer to access the implicit object and operator-> to select member m_id
+};
+
+struct Something {
+    int data{}; // member without m_, cause struct
+
+    void setData(int data) {
+        this->data = data; // using this for disambiguate data parameter from member
+    }
 };
 
 int main() {
