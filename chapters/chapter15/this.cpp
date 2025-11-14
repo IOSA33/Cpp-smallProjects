b1b446982d652749ac198bfae05dfbeaedea9e80
IOSA33 <sedinkin33@gmail.com>
2025-11-14 14:33:35 +0200

    hidden pointer in objects

---
+++ chapters/chapter15/this.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Simple {
+private:
+    int m_id{};
+
+public:
+    Simple(int id)
+        : m_id{ id } {
+    }
+
+    int getID() const { return m_id; }
+    void setID(int id) { m_id = id; }
+
+    void print() const { std::cout << this->m_id; } // use `this` pointer to access the implicit object and operator-> to select member m_id
+};
+
+int main() {
+    Simple simple{ 1 };
+    simple.setID(2);
+    // Compiler rewrites it as: Simple::setID(&simple, 2)
+
+    simple.print();
+
+    return 0;
+}
