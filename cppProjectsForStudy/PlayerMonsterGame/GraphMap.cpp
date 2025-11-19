9d37fc8fb4013f49438a3de3e96afc206e8ef590
IOSA33 <sedinkin33@gmail.com>
2025-11-19 22:08:43 +0200

    adde City class to the Grapth

--- cppProjectsForStudy/PlayerMonsterGame/GraphMap.cpp
+++ cppProjectsForStudy/PlayerMonsterGame/GraphMap.cpp
@@ -3,20 +3,24 @@
 #include <iostream>
 #include <bits/ostream.tcc>
 
-void GraphMap::setCityOnMap(const int x, const int y) {
-    std::cout << x << ", " << y << std::endl;
+void GraphMap::setCityOnMap(City* x, City* y) {
     // Adding the edge between x and y
     m_city[x].emplace_back(y);
     // Adding the edge between y and x, for undirected graph
     m_city[y].emplace_back(x);
+
+    if (m_city.contains(x) && !m_city[x].empty()) {
+        std::cout << x->getPopulation() << ", " << y->getPopulation() << std::endl;
+    }
+
 }
 
 void GraphMap::printGraph() const {
     std::cout << "Graph of the map" << std::endl;
     for (const auto&[fst, snd]: m_city) {
-        std::cout << fst << " -> ";
+        std::cout << fst->getName() << " -> ";
         for (const auto j: snd) {
-            std::cout << j << " ";
+            std::cout << "The road to the " << j->getName() << " is visited:" << j->isVisited() << ". ";
         }
         std::cout << std::endl;
     }
@@ -24,8 +28,18 @@ void GraphMap::printGraph() const {
 
 int main() {
     GraphMap g;
-    g.setCityOnMap(0, 1);
-    g.setCityOnMap(0, 2);
+
+    City* c1 = new City{"The castle", 12500, true};
+    City* c2 = new City{"The boiled", 405, false};
+    City* c3 = new City{"Closest sun", 1023, false};
+
+    g.setCityOnMap(c1, c2);
+    g.setCityOnMap(c1, c3);
     g.printGraph();
+
+    delete c1;
+    delete c2;
+    delete c3;
+
     return 0;
 }
--- cppProjectsForStudy/PlayerMonsterGame/GraphMap.h
+++ cppProjectsForStudy/PlayerMonsterGame/GraphMap.h
@@ -3,10 +3,20 @@
 
 #include <map>
 #include <list>
+#include <string>
 
-class City {
-    int population{};
-    bool visited{};
+class City{
+    std::string m_name{};
+    int m_population{};
+    bool m_visited { false };
+
+public:
+    City(const std::string_view name, const int population, const bool visited)
+    : m_name(name), m_population(population), m_visited(visited) {}
+
+    std::string getName() const { return m_name; }
+    int getPopulation() const {return m_population;};
+    bool isVisited() const {return m_visited;};
 };
 
 
@@ -14,10 +24,10 @@ class GraphMap {
     // We init a map with a first value as node and the second will be
     // a linked list that will represent other nodes that is connected
     // with the node.
-    std::map<int, std::list<int> > m_city;
+    std::map<City*, std::list<City*> > m_city;
 
 public:
-    void setCityOnMap(const int x, const int y);
+    void setCityOnMap(City* x, City* y);
     void printGraph() const;
 };
 
