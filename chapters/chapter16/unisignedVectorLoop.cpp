e0d73c964f5b36b06de644b7ac93d23bb58a8c51
IOSA33 <sedinkin33@gmail.com>
2025-11-22 23:41:01 +0200

    maybe the best option for indexing int in for loop

--- chapters/chapter16/unisignedVectorLoop.cpp
+++ chapters/chapter16/unisignedVectorLoop.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+// Good option, but in the main the best
 template<typename T>
 void printReverse(const std::vector<T>& vec) {
     for (int index{static_cast<int>(vec.size()) - 1}; index >= 0; --index) {
@@ -9,6 +10,24 @@ void printReverse(const std::vector<T>& vec) {
     std::cout << std::endl;
 }
 
+
+//     --------------- Best practise ---------------
+// Avoid array indexing with integral values whenever possible.
+
+
 int main() {
+    using Index = std::ptrdiff_t;
+
+    std::vector<int> arr{9,7,5,3,2,5};
+
+    // Best option for forloop indexing
+    auto length{static_cast<Index>(arr.size())};
+    for (auto index{ length - 1}; index >= 0; --index) {
+        // Since data() returns the array data as a C-style array,
+        // and C-style arrays allow indexing with both signed and
+        // unsigned values, this avoids sign conversion issues.
+        std::cout << arr.data()[index] << std::endl;
+    }
+
     return 0;
 }
