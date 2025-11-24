4d70ca8b52688b1bb711eef0920024b99346dca3
IOSA33 <sedinkin33@gmail.com>
2025-11-24 19:55:36 +0200

    added func to find from one node to another distance between them

--- algorithms/binaryTreeSearch.cpp
+++ algorithms/binaryTreeSearch.cpp
@@ -7,7 +7,7 @@ public:
     Node* m_left { nullptr };
     Node* m_right { nullptr };
 
-    explicit Node(int data) : m_data(data)
+    explicit Node(const int data) : m_data(data)
     {}
 };
 
@@ -15,10 +15,50 @@ class BinaryTree {
 private:
     Node* m_root { nullptr };
 
-    bool search(Node* current, int data) {
+    bool m_search(const Node* current, const int data) {
         if (current == nullptr) return false;
         if (current->m_data == data) return true;
-        return search(current->m_left, data) || search(current->m_right, data); 
+        return m_search(current->m_left, data) || m_search(current->m_right, data); 
+    }
+
+    int m_MaxHeight(const Node* current) {
+        if (current == nullptr) return 0;
+        int rightPath = m_MaxHeight(current->m_right);
+        int leftPath = m_MaxHeight(current->m_left);
+
+        return 1 + (leftPath > rightPath ? rightPath : leftPath);
+    }
+
+    int findLevel(const Node *current, const int k, const int level) {
+        if (current == nullptr) return -1;
+        if (current->m_data == k) return level;
+
+        int leftLevel = findLevel(current->m_left, k, level + 1);
+
+        if (leftLevel == -1) return findLevel(current->m_right, k, level + 1);
+        return leftLevel;
+    }
+    
+    Node* LCA(Node* current, int node1, int node2) {
+        if (current == nullptr) return current;
+        if (current->m_data == node1 || current->m_data == node2) return current;
+
+        Node* left = LCA(current->m_left, node1, node2);
+        Node* right = LCA(current->m_right, node1, node2);
+
+        if (left != nullptr && right != nullptr) return current;
+        if (left == nullptr && right == nullptr) return nullptr;
+
+        return (left != nullptr) ? LCA(current->m_left, node1, node2) : LCA(current->m_right, node1, node1); 
+    }
+
+    int m_distance(Node* current, int a, int b) {
+        Node* lca = LCA(current, a, b);
+
+        int d1 = findLevel(lca, a, 0);
+        int d2 = findLevel(lca, b, 0);
+
+        return d1 + d2;
     }
 
 public:
@@ -43,21 +83,41 @@ public:
                 current->m_left = newNode;
                 return;
             } else {
+                q.push(current->m_left);
+            }
+            if (current->m_right == nullptr) {
+                current->m_right = newNode;
+                return;
+            } else {
                 q.push(current->m_right);
             }
         }
     }
 
+    void search(int data) {
+        m_search(m_root, data);
+    }
 
-    void printLeftTree() {
-        while (m_root->m_left != nullptr) {
+    int height() {
+        return m_MaxHeight(m_root);
+    }
 
-        }
+    int distance(int x, int y) {
+        return m_distance(m_root, x, y);
     }
 };
 
 
 int main() {
+
+    BinaryTree tree{};
+    tree.insertNode(30);
+    tree.insertNode(20);
+    tree.insertNode(40);
+    tree.insertNode(10);
+
+    std::cout << tree.height() << std::endl;
+    std::cout << tree.distance(10, 40) << std::endl; // Needs to print 3
 
     return 0;
 }
