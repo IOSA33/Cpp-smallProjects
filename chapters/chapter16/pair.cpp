6ec5d01252ff273e0b1ca6a6e7fac62a5866e6ea
IOSA33 <sedinkin33@gmail.com>
2025-11-28 00:10:03 +0200

    added user input pair

--- chapters/chapter16/pair.cpp
+++ chapters/chapter16/pair.cpp
@@ -1,41 +1,51 @@
 #include <utility>
 #include <vector>
 #include <iostream>
+#include <limits>
 
 template <typename T>
-std::pair<std::pair<T, T>, std::pair<T, T>> returnPair(const std::vector<T>& v) {
-    T min{ v[0] };
-    T minIndex{}; 
-    T max{ v[0] };
-    T maxIndex{};
+std::pair<std::size_t, std::size_t> returnPair(const std::vector<T>& v) {
+    std::size_t minIndex{}; 
+    std::size_t maxIndex{};
 
-    for (size_t i { 0 }; i < v.size(); ++i) {
-        if (v[i] > max) {
-            minIndex = static_cast<T>(i);
-            max = v[i];
+    for (std::size_t i { 0 }; i < v.size(); ++i) {
+        if (v[i] < v[minIndex]) {
+            minIndex = i;
         }
-        if (v[i] < min) {
-            maxIndex = static_cast<T>(i);
-            min = v[i];
+        if (v[i] > v[maxIndex]) {
+            maxIndex = i;
         }
     }
 
-    return std::make_pair(std::make_pair(min, minIndex), std::make_pair(max, maxIndex));
+    return { minIndex, maxIndex };
 }
 
 int main() {
 
-    std::vector v1 { 3, 8, 2, 5, 7, 8, 3 };
-    std::vector v2 { 5.5, 2.7, 3.3, 7.6, 1.2, 8.8, 6.6 };
+    std::vector<int> v;
+    v.reserve(15);
+
+    while (true) {
+        std::cout << "Enter numbers to add (use -1 to stop): \n";
+        int x{};
+        std::cin >> x;
+
+        if (!std::cin) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max());
+            continue;
+        }
+
+        if (x == -1) {
+            break;
+        }
+
+        v.push_back(x);
+    }
     
-    auto pair1 { returnPair(v1) };
-    auto pair2 { returnPair(v2) };
+    auto pair1 { returnPair(v) };
 
-    std::cout << "The min element has index " << pair1.first.second<<" and value " <<pair1.first.first<<"\n";
-    std::cout << "The max element has index " << pair1.second.second<<" and value " <<pair1.second.first<<"\n";
-    std::cout << std::endl;
-    std::cout << "The min element has index " << pair2.first.second<<" and value " <<pair2.first.first<<"\n";
-    std::cout << "The max element has index " << pair2.second.second<<" and value " <<pair2.second.first<<"\n";
-
+    std::cout << "The min element has index " << pair1.first<<" and value " <<v[pair1.first]<<"\n";
+    std::cout << "The max element has index " << pair1.second<<" and value " <<v[pair1.second]<<"\n";
     return 0;
 }
