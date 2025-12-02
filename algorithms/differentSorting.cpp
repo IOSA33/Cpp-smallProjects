d28685490f485b21dfe1bf375ede41109b98df36
IOSA33 <sedinkin33@gmail.com>
2025-12-02 21:24:57 +0200

    insertion algorithm

--- algorithms/differentSorting.cpp
+++ algorithms/differentSorting.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 
-void couple(std::string& word) {
+void bubble(std::string& word) {
     auto length{static_cast<std::ptrdiff_t>(word.size())};
     for (size_t i {0}; i < word.size(); ++i) {
         for (auto j { length - 2 }; j >= 0; --j) {
@@ -15,10 +15,24 @@ void couple(std::string& word) {
     }
 }
 
+void inseriton(std::string& word) {
+    for (size_t i {0}; i < word.size(); ++i) {
+        const char temp = word[i];
+        int j = static_cast<int>(i) - 1;
+        
+        while (j >= 0 && word[j] > temp) {
+            word[j + 1] = word[j];
+            j -= 1;
+        }
+
+        word[j + 1] = temp;
+    }
+}
+
 int main() {
 
     std::string word { "REKURSIO" };
-    couple(word);
+    inseriton(word);
     std::cout << word;
 
     return 0;
