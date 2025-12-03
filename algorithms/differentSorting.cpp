78def680c3f08fa2895501ad569d110eeecb89bd
IOSA33 <sedinkin33@gmail.com>
2025-12-03 18:07:11 +0200

    selection sort

--- algorithms/differentSorting.cpp
+++ algorithms/differentSorting.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <ranges>
+
 
 
 void bubble(std::string& word) {
-    auto length{static_cast<std::ptrdiff_t>(word.size())};
-    for (size_t i {0}; i < word.size(); ++i) {
-        for (auto j { length - 2 }; j >= 0; --j) {
+    auto length{std::ranges::ssize(word)};
+    for (int i { 0 }; i < length; ++i) {
+        for (auto j { length - 2 }; j >= i - 1; --j) {
             if (word.data()[j] > word.data()[j + 1]){
                 auto temp = word.data()[j];
                 word.data()[j] = word.data()[j + 1];
@@ -29,10 +31,30 @@ void inseriton(std::string& word) {
     }
 }
 
+
+void selection(std::string& word) {
+    for (size_t i = 0; i < word.size(); ++i) {
+        char temp = word[i];
+        size_t k = i;
+        for(size_t j = i + 1; j < word.size(); ++j) {
+            if (word[j] < temp) {
+                temp = word[j];
+                k = j;
+            }
+        }
+        if (k != i) {
+            word[k] = word[i];
+            word[i] = temp;
+        }
+    }
+}
+
+
 int main() {
 
     std::string word { "REKURSIO" };
-    inseriton(word);
+    selection(word);
+    std::cout << word << "\n";
     std::cout << word;
 
     return 0;
