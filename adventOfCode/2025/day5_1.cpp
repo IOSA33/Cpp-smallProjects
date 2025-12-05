e4314b0a53e65238ef78146d85c80cbbb0858ca9
IOSA33 <sedinkin33@gmail.com>
2025-12-05 22:46:58 +0200

    second part

--- adventOfCode/2025/day5_1.cpp
+++ adventOfCode/2025/day5_1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <algorithm>
 #include <sstream>
 
 void solveFirst() {
@@ -11,7 +12,8 @@ void solveFirst() {
         std::exit(1);
     }
 
-    int result{};
+    // First part
+    int fresh{};
     std::vector<long long> ranges{};
 
     std::string line{};
@@ -25,11 +27,12 @@ void solveFirst() {
                 while (std::getline(ss, t, del)) {
                     ranges.push_back(std::stoll(t));
                 }
+
             } else {
                 long long int temp = std::stoll(line);
                 for (size_t i{ 0 }; i < ranges.size(); i += 2) {
                     if (temp >= ranges[i] && temp <= ranges[i + 1]) {
-                        ++result;
+                        ++fresh;
                         break;
                     }
                 }
@@ -37,8 +40,30 @@ void solveFirst() {
         }
     }
 
+    // second part
+    std::vector<std::pair<long long, long long>> ranges2{};
+    for (size_t i = 0; i < ranges.size(); i += 2) {
+        ranges2.push_back({ranges[i], ranges[i + 1]});
+    }
+    std::ranges::sort(ranges2);
+
+    std::vector<std::pair<long long, long long>> merged;
+    for (const auto& range : ranges2) {
+        if (merged.empty() || range.first > merged.back().second + 1) {
+            merged.push_back(range);
+        } else {
+            merged.back().second = std::max(merged.back().second, range.second);
+        }
+    }
+
+    long long total = 0;
+    for (const auto& m : merged) {
+        total += (m.second - m.first + 1);
+    }
+
     file.close();
-    std::cout << result;
+    std::cout << fresh << std::endl;
+    std::cout << total << std::endl;
 }
 
 
