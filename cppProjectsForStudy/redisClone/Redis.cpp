c0da87af191426eb91f662682365ddefa85cd273
IOSA33 <sedinkin33@gmail.com>
2025-12-28 22:04:19 +0200

    v0.1 set and get commands

--- cppProjectsForStudy/redisClone/Redis.cpp
+++ cppProjectsForStudy/redisClone/Redis.cpp
@@ -1,15 +1,67 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include "Redis.h"
 
+// Commands:
+// SET [key] [value] : returns "OK"
+// GET [key] : returns "value"
+
 void Redis::run() {
     while(true) {
-        int input {};
-        std::cin >> input;
+        std::cout << "Write a command SET [key] [value] or GET [key]\n";
+        std::string input{};
+        std::getline(std::cin >> std::ws, input);
 
-        if (input == 1) {
-            std::cout << 1;
-            return;
+        const std::string response { parser(input) };
+
+        std::cout << response << '\n';
+    }
+}
+
+const std::string Redis::parser(const std::string& s) {
+    std::vector<std::string> vec{};
+    std::string word{};
+
+    for (const auto& i: s) {
+        if (i != ' ') {
+            word.push_back(i);
+            continue;
+        }
+        vec.emplace_back(word);
+        word.clear();
+    }
+
+    if (!word.empty()) {
+        vec.emplace_back(word);
+    }
+
+    if (vec[0] == "SET") {
+        if (vec.size() > 2) {
+           setValue(vec[1], vec[2]);
+           return "OK";
+        } else {
+            return "SET has less than 3 arguments";
+        }
+    } else if (vec[0] == "GET") {
+        if (vec.size() >= 2) {
+            return getValue(vec[1]);
+        } else {
+            return "GET has less than 2 args";
         }
     }
+
+    return "Error in Redis::parser(): unknow Command";
+}
+
+void Redis::setValue(const std::string& key, const std::string& value) {
+    m_umap.insert({key, value});
+}
+
+std::string Redis::getValue(const std::string& key) const {
+    auto it { m_umap.find(key) };
+    if (it != m_umap.end()) {
+        return it->second;
+    }
+    return "Error getValue(): no such Key";
 }
--- cppProjectsForStudy/redisClone/Redis.h
+++ cppProjectsForStudy/redisClone/Redis.h
@@ -6,11 +6,15 @@ private:
     std::unordered_map<std::string, std::string> m_umap{};
     
 public:
-    // TODO: textfile constructor
+    // TODO: textfile path constructor
     Redis() = default;
     ~Redis() {
     //    writeToLog();
     }
 
     void run();
+    // Returns response code
+    const std::string parser(const std::string& input);
+    void setValue(const std::string& key, const std::string& value);
+    std::string getValue(const std::string& key) const;
 };
