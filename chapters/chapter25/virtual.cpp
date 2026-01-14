90c12c224ecc8bfcb1f2872b86681809c7997238
IOSA33 <sedinkin33@gmail.com>
2026-01-14 21:13:42 +0200

    virtual basics

---
+++ chapters/chapter25/virtual.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Animal {
+private:
+    std::string m_name{};
+
+public:
+    virtual std::string_view getName() { return "ANIMAL"; }
+};
+
+class Dog : public Animal{
+public:
+    std::string_view getName() { return "DOG"; }
+};
+
+class Poodle : public Dog {
+public:
+    virtual std::string_view getName() { return "POODLE"; }
+};
+
+int main() {
+    Dog dog{};
+    Animal& rAnimal{ dog };
+    std::cout << rAnimal.getName() << std::endl;
+}
